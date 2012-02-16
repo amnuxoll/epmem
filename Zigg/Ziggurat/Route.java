d828c5e1d30dbb9afbf3df351ec2d9a2ef94ad64
Zachary Faltersack <faltersa10@up.edu>
2012-02-15 19:55:41 -0800

    I have everything separated into two directories: UnitTest, Ziggurat Each contains what you'd expect.

---
+++ Zigg/Ziggurat/Route.java
@@ -0,0 +1,90 @@
+package Ziggurat;
+
+import java.util.*;
+
+
+public class Route
+{
+	private Vector<Sequence> sequences;
+	private Sequence replSeq;
+	private int currSeqIndex;
+	private int currActIndex;
+
+	public Route() 
+    {
+        sequences = new Vector<Sequence>();
+        replSeq = null;
+        currSeqIndex = -1;
+        currActIndex = -1;
+	}
+
+	public void Route(Vector<Sequence> initSeq) 
+    {
+        sequences = initSeq == null ? new Vector<Sequence>() : initSeq;
+        replSeq = null;
+        currSeqIndex = -1;
+        currActIndex = -1;
+		
+	}
+
+	public String toString() 
+    {
+		return "";
+	}
+
+	public String toStringLong() 
+    {
+		return "";
+	}
+
+	/** auto increments tthe currAction/currSequence pointers	 */
+	public Action nextAction() 
+    {
+		currActIndex++;
+        if(currActIndex >= sequences.elementAt(currSeqIndex).length())
+        {
+            currActIndex = 0;
+            currSeqIndex++;
+            if(currSeqIndex >= sequences.size())
+            {
+                return null;
+            }
+        }
+        return sequences.elementAt(currSeqIndex).getActionAtIndex(currActIndex);
+	}
+
+	public Action getCurrAction() 
+    {
+        return sequences.elementAt(currSeqIndex).getActionAtIndex(currActIndex);
+	}
+
+	public Sequence getCurrSequence() 
+    {
+        return sequences.elementAt(currSeqIndex);
+	}
+
+	public void applyReplacement(Replacement repl) 
+    {
+		
+	}
+
+	public int numElementalEpisodes() 
+    {
+        int count = 0;
+        for(Sequence s : sequences)
+        {
+            count += s.numElementalEpisodes();
+        }
+		return count;
+	}
+
+	public int remainingElementalEpisodes() 
+    {
+        int count = 0;
+        for(int i = currSeqIndex; i < sequences.size(); ++i)
+        {
+            count += sequences.elementAt(i).numElementalEpisodes();
+        }
+		return count - currActIndex;
+	}
+}//class Route
