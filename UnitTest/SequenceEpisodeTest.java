65a07af16287fd0d040e8c706377341ef35c9169
Zachary Faltersack <faltersa10@up.edu>
2012-04-10 14:39:34 -0700

    I had to fix a bad merge on the file I forgot to add.

--- UnitTest/SequenceEpisodeTest.java
+++ UnitTest/SequenceEpisodeTest.java
@@ -1,4 +1,3 @@
-<<<<<<< local
 package UnitTest;
 
 import org.junit.*;
@@ -35,43 +34,3 @@ public class SequenceEpisodeTest {
 		//assertFalse(s == seep1);
 	}
 }
-=======
-/** A dummy class to fill in until Zach checks in the real deal */
-package UnitTest;
-
-import Ziggurat.Episode;
-
-public class SequenceEpisodeTest extends Episode
-{
-    String name = "foo";
-    
-    public static SequenceEpisodeTest seep1 = new SequenceEpisodeTest("seep1");
-    public static SequenceEpisodeTest seep2 = new SequenceEpisodeTest("seep2");
-    public static SequenceEpisodeTest seep3 = new SequenceEpisodeTest("seep3");
-
-    public SequenceEpisodeTest() {}
-
-    public SequenceEpisodeTest(String s)
-    {
-        name = s;
-    }
-    
-    public SequenceEpisodeTest clone()
-    {
-        return new SequenceEpisodeTest();
-    }
-
-    public String toString() { return name; }
-    public boolean equals(Episode other)
-    {
-        if (! (other instanceof SequenceEpisodeTest))
-        {
-            return false;
-        }
-
-        return ( ((SequenceEpisodeTest)other).name.equals(this.name));
-    }
-
-
-}
->>>>>>> other
