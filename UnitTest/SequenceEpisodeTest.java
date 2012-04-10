9321932e2b8ebada10d5d2fe98d4c384b7ec07f4
Zachary Faltersack <faltersa10@up.edu>
2012-04-10 14:35:35 -0700

    I have added back in the files I forgot to add to the repository. I don't know how I forgot because after I lost all those files on my home machine I was being really careful to pay attention to that stuff.
    Sorry about the inconvenience.

--- UnitTest/SequenceEpisodeTest.java
+++ UnitTest/SequenceEpisodeTest.java
@@ -1,3 +1,41 @@
+<<<<<<< local
+package UnitTest;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import Ziggurat.Sequence;
+import Ziggurat.SequenceEpisode;
+
+public class SequenceEpisodeTest {
+
+	public static SequenceEpisode seep1 = new SequenceEpisode(SequenceTest.sEE1);
+	public static SequenceEpisode seep2 = new SequenceEpisode(SequenceTest.sEE2);
+	public static SequenceEpisode seep3 = new SequenceEpisode(SequenceTest.sEE3);
+	
+	@Test
+	public void test_equals()
+	{
+		//assertTrue(seep1.equals(seep2));
+		//assertFalse(seep1.equals(seep3));
+	}
+	
+	@Test
+	public void test_getSequence()
+	{
+		//assertTrue(seep1.getSequence().equals(SequenceTest.sEE1));
+	}
+	
+	@Test
+	public void test_clone()
+	{
+		//SequenceEpisode s = seep1.clone();
+		//assertTrue(s.equals(seep1));
+		//assertFalse(s == seep1);
+	}
+}
+=======
 /** A dummy class to fill in until Zach checks in the real deal */
 package UnitTest;
 
@@ -36,3 +74,4 @@ public class SequenceEpisodeTest extends Episode
 
 
 }
+>>>>>>> other
